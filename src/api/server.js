c2325e0cedd184bf2fc04bbbc5f729132c4f2c33
fhdyt <fhidayat2206@gmail.com>
2020-09-08 15:24:02 +0700

    test

--- src/api/server.js
+++ src/api/server.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
- baseURL: 'https://api-huawei.zte.my.id'
+ // baseURL: 'https://api-huawei.zte.my.id'
+ baseURL: 'http://7074c792868a.ngrok.io'
 });
   
